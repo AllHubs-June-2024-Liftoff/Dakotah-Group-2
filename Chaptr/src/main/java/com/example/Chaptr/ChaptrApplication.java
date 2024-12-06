a3c01ffc3ae529d9226ba6b4fb1b9e1963b845f7
Jillian Fusco <fuscojillian@gmail.com>
2024-12-05 21:51:51 -0500

    initial commit backend

---
+++ Chaptr/src/main/java/com/example/Chaptr/ChaptrApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chaptr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaptrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaptrApplication.class, args);
+	}
+
+}
---
+++ Chaptr/src/test/java/com/example/Chaptr/ChaptrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chaptr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaptrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
