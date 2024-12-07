abb8fc47e9faf0e866fc9706260081b959b68b84
jwaynick99 <jwaynick99@gmail.com>
2024-12-06 21:37:11 -0600

    Basic Models for Club and Book objects, hopefully on my dev branch.

---
+++ Chaptr/src/main/java/com/example/Chaptr/models/Club.java
@@ -0,0 +1,50 @@
+package com.example.Chaptr.models;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import org.hibernate.engine.internal.Cascade;
+
+import java.util.ArrayList;
+
+@Entity
+public class Club extends AbstractEntity{
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Book bookOfTheMonth;
+
+    private String clubMessage;
+
+    private final ArrayList<User> members = new ArrayList<>();
+
+    public Club(Book bookOfTheMonth, String clubMessage) {
+        super();
+        this.bookOfTheMonth = bookOfTheMonth;
+        this.clubMessage = clubMessage;
+    }
+
+    public void addMember(User newMember){
+        members.add(newMember);
+    }
+
+    public ArrayList<User> getMembers() {
+        return members;
+    }
+
+    public Book getBookOfTheMonth() {
+        return bookOfTheMonth;
+    }
+
+    public void setBookOfTheMonth(Book bookOfTheMonth) {
+        this.bookOfTheMonth = bookOfTheMonth;
+    }
+
+    public String getClubMessage() {
+        return clubMessage;
+    }
+
+    public void setClubMessage(String clubMessage) {
+        this.clubMessage = clubMessage;
+    }
+}
