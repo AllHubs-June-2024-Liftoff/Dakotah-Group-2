3a614591be235f2644bc8c568177c5bb1d9c44a7
Katie Russo <krusso196@gmail.com>
2024-12-16 22:06:25 -0500

    TBR Entity created

--- Chaptr/src/main/java/com/example/Chaptr/models/TBR.java
+++ Chaptr/src/main/java/com/example/Chaptr/models/TBR.java
@@ -1,14 +1,17 @@
 package com.example.Chaptr.models;
 
+//import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 
 import java.util.List;
-//
+
 @Entity
 public class TBR extends AbstractEntity{
 
     private List<Book> tbrBooks;
 
+    @OneToOne //not including (cascade = cascadeType.ALL) because a CRUD action on TBR should not cascade to User
     private User user; //foreign key
 
     public TBR(User user, List<Book> tbrBooks){
@@ -16,6 +19,8 @@ public class TBR extends AbstractEntity{
         this.user = user;
         this.tbrBooks = tbrBooks;
     }
+
+    public TBR(){}
 
     public void addToTBR(Book bookToAdd){
         if (!(tbrBooks.contains(bookToAdd))){
