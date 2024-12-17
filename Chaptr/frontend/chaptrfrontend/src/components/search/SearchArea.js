b27cf6b928e4daeef31d54b5ab1be6fc506bddb3
Jillian Fusco <fuscojillian@gmail.com>
2024-12-17 14:55:04 -0500

    added text input and button to SearchArea

--- Chaptr/frontend/chaptrfrontend/src/components/search/SearchArea.js
+++ Chaptr/frontend/chaptrfrontend/src/components/search/SearchArea.js
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function SearchArea(){
     return (
-        <div>
+        <div className="search-area">
             <h2>Search Area</h2>
-            
+            <form>
+                <input type="text">
+                
+                </input>
+                <button type="submit">Search</button>
+            </form>
         </div>
     );
 
