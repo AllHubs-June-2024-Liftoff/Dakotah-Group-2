76d4d085dc9d058600d26c0a9ced213b453ec864
Jillian Fusco <fuscojillian@gmail.com>
2024-12-23 12:39:09 -0500

    added ternary in BookCard to clean author names and published date

--- Chaptr/frontend/chaptrfrontend/src/components/search/BookCard.js
+++ Chaptr/frontend/chaptrfrontend/src/components/search/BookCard.js
@@ -6,8 +6,8 @@ export default function BookCard(props){
             <img src={props.image} alt="book cover"></img>
             <div className="desc">
                 <h2>{props.title}</h2>
-                <h3>{props.author}</h3>
-                <p>{props.publishedDate}</p>
+                <h3>{Array.isArray(props.author) ? props.author.join(", ") : props.author}</h3>
+                <p>{props.publishedDate === '0000' ? 'Not available' : props.publishedDate.substring(0,4)}</p>
             </div>
             <button>Add to TBR</button>
         </div>
