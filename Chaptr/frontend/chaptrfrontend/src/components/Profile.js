58923f64b3a04760bee08868fb40aab5445cdaea
scarfinor <scarfinor@gmail.com>
2024-12-22 21:29:36 -0600

    git commit post request code for addToTbr button

--- Chaptr/frontend/chaptrfrontend/src/components/Profile.js
+++ Chaptr/frontend/chaptrfrontend/src/components/Profile.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useEffect /* useState */ } from "react";
 import Button from "@mui/material/Button";
 import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios"
 
 export default function Profile() {
-  const Profile = ({ darkMode }) => {
-    const navigate = useNavigate();
+  /* const [book, setBook] = useState({
+    name: "",
+    author: "",
+    book_cover: "",
+    publication_date: "",
+  }); */
 
-    const user = JSON.parse(localStorage.getItem("user"));
+  const navigate = useNavigate();
 
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
     if (!user) {
       navigate("/Chaptr");
     }
-  };
+  }, [user, navigate]);
+
+  if (!user) {
+    return null;
+  }
+
+  /* const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/tbr", book);
+    setBook({ ...book, [e.target.name]: e.target.value });
+    navigate("/Profile");
+  }; */
 
   return (
     <div>
-      <h1>User Profile</h1>
+      <h1>{`${user.name}'s profile`}</h1>
 
       <Button
+        /* onClick={(e) => onSubmit(e)} */
+        /* type="submit" */
+        /* value={book} */
         variant="contained"
         component={Link}
         sx={{
