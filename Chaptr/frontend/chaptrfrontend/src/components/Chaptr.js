c8b97b027e85dda699ca2d155ef15608cefad586
Jillian Fusco <fuscojillian@gmail.com>
2025-01-19 13:57:00 -0500

    added book logo

--- Chaptr/frontend/chaptrfrontend/src/components/Chaptr.js
+++ Chaptr/frontend/chaptrfrontend/src/components/Chaptr.js
@@ -15,10 +15,12 @@ export default function Chaptr({ darkMode }) {
     <div className="chaptr-container">
       <div className="chaptr-desc">
         <div className="logo-title">
-          <i class="fa-regular fa-book"></i>
-          <h1>Chaptr</h1>
+          {/*book logo */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
+        <path d="M96 32C43 32 0 75 0 128V384C0 437 43 480 96 480H448V32H96zM384 384H96C69.5 384 48 362.5 48 336S69.5 288 96 288H384V384z" fill="currentColor"/>
+        </svg>
+        <h1>Chaptr</h1>
         </div>
-
 
         <h2>Discover your next favorite book</h2>
         <p>Search through over 40 million books to start your next adventure</p>
