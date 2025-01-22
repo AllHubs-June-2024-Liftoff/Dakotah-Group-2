fff81f2ebc014c633cbd4ff9dec4a4119348f491
Jillian Fusco <fuscojillian@gmail.com>
2025-01-22 13:39:51 -0500

    added ThemeColors.js to styles folder

---
+++ Chaptr/frontend/chaptrfrontend/src/styles/ThemeColors.js
@@ -0,0 +1,14 @@
+export const colors = {
+    purple: "#7A43C7",
+    pink: "#C54485",
+    blue: "#73A4AB",
+    black: "#121212",
+    darkGrey: "#444444",
+    lightGrey: "#e0e0e0",
+    blueGrey: "#333",
+    silverGrey: "#CCCCCC",
+    white: "ffffff",
+    offWhite: "#F2E5D7",    
+  };
+
+  /*old light blue #92B9BD */
