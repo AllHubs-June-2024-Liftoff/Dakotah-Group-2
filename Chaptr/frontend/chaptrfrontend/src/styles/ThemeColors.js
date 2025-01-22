23ae9d48222989e87932b07d53cca5a71d123b1f
Jillian Fusco <fuscojillian@gmail.com>
2025-01-22 13:14:15 -0500

    added ThemeColors.js and changed the colors to variables in Login.js

---
+++ Chaptr/frontend/chaptrfrontend/src/styles/ThemeColors.js
@@ -0,0 +1,15 @@
+
+export const colors = {
+    purple: "#7A43C7",
+    pink: "#C54485",
+    blue: "#73A4AB",
+    black: "#121212",
+    darkGrey: "#444444",
+    lightGrey: "e0e0e0",
+    blueGrey: "333",
+    white: "ffffff",
+    offWhite: "#F2E5D7",
+    lightGrey: "e0e0e0",
+    silverGrey: "#CCCCCC"
+  };
+  
