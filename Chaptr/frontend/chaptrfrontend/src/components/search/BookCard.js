dd04e714b56079f53aa86a03c27145029dfb34bb
Katie Russo <krusso196@gmail.com>
2025-01-27 22:47:39 -0500

    Book Card updated with favorites list BUT arrow function is missing something ;

--- Chaptr/frontend/chaptrfrontend/src/components/search/BookCard.js
+++ Chaptr/frontend/chaptrfrontend/src/components/search/BookCard.js
@@ -4,6 +4,50 @@ import Button from "@mui/material/Button";
 import { colors } from "../../styles/ThemeColors";
 
 export default function BookCard(props) {
+  const addToFavorites = asyn () => {
+    const storedUser = JSON.parse(sessionStorage.getItem("user"));
+
+    if (!storedUser) {
+      alert("User is not logged in.");
+      return;
+    }
+    const formattedAuthor = Array.isArray(props.author)
+      ? props.author
+      : [props.author];
+    const bookData = {
+      id: props.id,
+      name: props.title,
+      author: formattedAuthor,
+      bookCover: props.image,
+      publicationDate: props.publishedDate,
+    };
+    console.log("Sending book data:", bookData);
+
+    try {
+        const newBookResponse = await axios.post("http://localhost:8080/addBook", bookData);
+        console.log("Bok added:", newBookResponse.data);
+        
+        const userBook = newBookResponse.data;
+        const favoritesResponse = await axios.get(`http://localhost:8080/favorites/email/${storedUser.email}`);
+
+        if (favoritesResponse.status === 404 || !favoritesResponse.data || !favoritesResponse.data.id) {
+            console.log(`Creating new Favorites List for: ${storedUser.name}`);
+            const newFavoritesResponse = await axios.post(`http://localhost:8080/newFavorites/email/${storedUser.email}`, {bookId: userBook.id});
+            console.log("New Favorites List created:", newFavoritesResponse.data);
+            alert("Book added to Favorites List");
+        } else {
+            const addToFavoritesResponse = await axios.put(`http://localhost:8080/favorites/${storedUser.email}`,userBook);
+            
+            if (addToFavoritesResponse.status === 200) {
+                alert(`${userBook.name} added to ${storedUser.name}'s Favorites List`);
+            }
+        }
+    } catch (error) {
+        console.error("Error adding book to TBR:", error);
+        alert("An error occurred while adding the book.");
+      }
+    };
+
   const addToTBR = async () => {
     const storedUser = JSON.parse(sessionStorage.getItem("user"));
 
@@ -76,13 +120,19 @@ export default function BookCard(props) {
             : props.publishedDate.substring(0, 4)}
         </p>
       </div>
-      {/* <button >Add to TBR</button> */}
       <Button
           variant="contained"
           onClick={addToTBR}
           sx={{ marginRight: 2, backgroundColor: colors.blue }}
           >
           Add to TBR
+      </Button>
+      <Button
+          variant="contained"
+          onClick={addToFavorites}
+          sx={{ marginRight: 2, backgroundColor: colors.blue }}
+          >
+          Add to Favorites
       </Button>
 </div>
   );
