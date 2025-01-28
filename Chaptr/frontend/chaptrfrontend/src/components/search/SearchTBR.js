b6a9b4016d7a6c8463cb44a3c564dc31c21080bb
Katie Russo <krusso196@gmail.com>
2025-01-27 22:46:49 -0500

    SearchTBR card updated to say Search Page

--- Chaptr/frontend/chaptrfrontend/src/components/search/SearchTBR.js
+++ Chaptr/frontend/chaptrfrontend/src/components/search/SearchTBR.js
@@ -9,7 +9,7 @@ export default function SearchTBR() {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
                 <path d="M96 32C43 32 0 75 0 128V384C0 437 43 480 96 480H448V32H96zM384 384H96C69.5 384 48 362.5 48 336S69.5 288 96 288H384V384z" fill="currentColor"/>
                 </svg>
-                <h1>Add to TBR</h1>
+                <h1>Search Page</h1>
             </div>
            
             <Books />
