29630036157e7a90c93664ecc92a6f268140cb72
Jillian Fusco <fuscojillian@gmail.com>
2025-01-28 15:58:14 -0500

    Added ThemeColors.js to /styles

---
+++ Chaptr/frontend/chaptrfrontend/src/styles/ThemeColors.js
@@ -0,0 +1,20 @@
+export const colors = {
+  purple: "#7A43C7",
+  pink: "#C54485",
+  blue: "#73A4AB",
+  //   the below colors go from darkest to lightest
+  black: "#121212",
+  blueGrey: "#333",
+  darkGrey: "#444444",
+  silverGrey: "#CCCCCC",
+  lightGrey: "#e0e0e0",    
+  whitesmoke: "#f5f5f5",
+  whitersmoke: "#fafafa",
+  white: "ffffff",    
+};
+
+
+//   old colors
+//   old purple #9B4DFF
+// old pink #FF1493
+// old light blue #92B9BD
