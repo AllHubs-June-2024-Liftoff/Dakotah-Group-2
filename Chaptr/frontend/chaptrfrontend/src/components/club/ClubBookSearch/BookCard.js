ad76fb89fa7dc94bd5ccdeb77ff084e050a750d0
scarfinor <scarfinor@gmail.com>
2025-01-29 21:44:06 -0600

    BookCard.js (change add to button from TBR to Club) imageDirectory.java (changed where profile pictures save to)

--- Chaptr/frontend/chaptrfrontend/src/components/club/ClubBookSearch/BookCard.js
+++ Chaptr/frontend/chaptrfrontend/src/components/club/ClubBookSearch/BookCard.js
@@ -3,35 +3,46 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export default function BookCard(props) {
-    const navigate = useNavigate();
-    const setClubBook = async () => {
-        const clubId = sessionStorage.getItem("clubId");
+  const navigate = useNavigate();
+  const setClubBook = async () => {
+    const clubId = sessionStorage.getItem("clubId");
 
-        const formattedAuthor = Array.isArray(props.author) ? props.author : [props.author];
-        const bookData = {
-            id: props.id,
-            name: props.title,
-            author: formattedAuthor,
-            bookCover: props.image,
-            publicationDate: props.publishedDate,
-        };
-        console.log("Sending book data:", bookData);
-
-        const newBookResponse = await axios.post(`http://localhost:8080/club/${clubId}/book`, bookData);
-        console.log("Book added:", newBookResponse.data);
-
-        navigate("/Club");
+    const formattedAuthor = Array.isArray(props.author)
+      ? props.author
+      : [props.author];
+    const bookData = {
+      id: props.id,
+      name: props.title,
+      author: formattedAuthor,
+      bookCover: props.image,
+      publicationDate: props.publishedDate,
     };
+    console.log("Sending book data:", bookData);
 
-    return (
-        <div className="card-container">
-            <img src={props.image} alt="book cover" />
-            <div className="desc">
-                <h2>{props.title}</h2>
-                <h3>{Array.isArray(props.author) ? props.author.join(", ") : props.author}</h3>
-                <p>{props.publishedDate === "0000" ? "Not available" : props.publishedDate.substring(0, 4)}</p>
-            </div>
-            <button onClick={setClubBook}>Add to TBR</button>
-        </div>
+    const newBookResponse = await axios.post(
+      `http://localhost:8080/club/${clubId}/book`,
+      bookData
     );
+    console.log("Book added:", newBookResponse.data);
+
+    navigate("/Club");
+  };
+
+  return (
+    <div className="card-container">
+      <img src={props.image} alt="book cover" />
+      <div className="desc">
+        <h2>{props.title}</h2>
+        <h3>
+          {Array.isArray(props.author) ? props.author.join(", ") : props.author}
+        </h3>
+        <p>
+          {props.publishedDate === "0000"
+            ? "Not available"
+            : props.publishedDate.substring(0, 4)}
+        </p>
+      </div>
+      <button onClick={setClubBook}>Add to Club</button>
+    </div>
+  );
 }
--- Chaptr/src/main/java/com/example/Chaptr/services/imageDirectory.java
+++ Chaptr/src/main/java/com/example/Chaptr/services/imageDirectory.java
@@ -2,6 +2,6 @@ package com.example.Chaptr.services;
 
 public class imageDirectory {
 
-    public static final String IMAGE_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String IMAGE_DIRECTORY = System.getProperty("user.home") + "/Desktop/ChaptrStorage/";
 
 }
