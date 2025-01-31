92e7c5d95a96d0f194856eb597af261490cc54bd
jwaynick99 <jwaynick99@gmail.com>
2025-01-31 14:45:06 -0600

    Showing Clubs List on Profile Owner page

--- Chaptr/frontend/chaptrfrontend/src/components/users/ProfileOwner.js
+++ Chaptr/frontend/chaptrfrontend/src/components/users/ProfileOwner.js
@@ -1,167 +1,213 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import axios from "axios";
+import Button from "@mui/material/Button";
+import { colors } from "../../styles/ThemeColors";
 
 export default function ProfileOwner({ darkMode }) {
-  const [owner, setOwner] = useState(null);
-  const [tbr, setTbr] = useState({ tbr: [] });
-  const { id } = useParams();
+    const [owner, setOwner] = useState(null);
+    const [tbr, setTbr] = useState({ tbr: [] });
+    const [userClubs, setUserClubs] = useState([]);
+    const { id } = useParams();
 
-  useEffect(() => {
-    if (id) {
-      const getProfileOwner = async () => {
-        try {
-          const response = await axios.get(`http://localhost:8080/owner/${id}`);
-          setOwner(response.data);
-          if (response.data.email) {
-            loadTBRLists(response.data.email);
-          }
-        } catch (error) {
-          console.error("Error fetching owner data:", error);
+    const navigate = useNavigate();
+
+    const getUserClubs = async () => {
+        const response = await axios.get(`http://localhost:8080/clubs/${id}`);
+        setUserClubs(response.data);
+        console.log(response.data);
+    };
+
+    useEffect(() => {
+        if (id) {
+            const getProfileOwner = async () => {
+                try {
+                    const response = await axios.get(`http://localhost:8080/owner/${id}`);
+                    setOwner(response.data);
+                    getUserClubs();
+                    if (response.data.email) {
+                        loadTBRLists(response.data.email);
+                    }
+                } catch (error) {
+                    console.error("Error fetching owner data:", error);
+                }
+            };
+
+            getProfileOwner();
         }
-      };
+    }, [id]);
 
-      getProfileOwner();
+    const loadTBRLists = async (userEmail) => {
+        try {
+            const response = await axios.get(`http://localhost:8080/tbr/email/${userEmail}`);
+            setTbr(response.data);
+        } catch (error) {
+            console.error("Error fetching TBR Lists:", error);
+        }
+    };
+
+    if (!owner) {
+        return <div>Loading owner data...</div>;
     }
-  }, [id]);
 
-  const loadTBRLists = async (userEmail) => {
-    try {
-      const response = await axios.get(
-        `http://localhost:8080/tbr/email/${userEmail}`
-      );
-      setTbr(response.data);
-    } catch (error) {
-      console.error("Error fetching TBR Lists:", error);
-    }
-  };
+    const clubButton = async (clubId) => {
+        sessionStorage.setItem("clubId", clubId);
+        navigate("/Club");
+    };
 
-  if (!owner) {
-    return <div>Loading owner data...</div>;
-  }
+    return (
+        <div>
+            <div className="user-profile-display edit-user-profile-display">
+                <img
+                    src={owner.userImage || "path/to/default/image.jpg"}
+                    alt={owner.firstName}
+                    style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+                />
+                <h1>{`${owner.firstName + " " + owner.lastName}`}</h1>
+            </div>
+            <div className="profile-clubs-container">
+                <div className="profile-clubs">
+                    <h1>My Clubs</h1>
+                    {userClubs.length > 0 ? (
+                        userClubs.map((club) => (
+                            <div>
+                                <Button
+                                    key={club.id}
+                                    onClick={() => clubButton(club.id)}
+                                    variant="contained"
+                                    style={{ backgroundColor: colors.purple }}
+                                >
+                                    {club.name}
+                                </Button>
+                            </div>
+                        ))
+                    ) : (
+                        <p>Not a member of any clubs yet!</p>
+                    )}
 
-  return (
-    <div>
-      <div className="user-profile-display edit-user-profile-display"> 
-        <img
-            src={owner.userImage || "path/to/default/image.jpg"}
-            alt={owner.firstName}
-            style={{ width: "100px", height: "100px", borderRadius: "50%" }}
-          />
-        <h1>{`${owner.firstName + " " + owner.lastName}`}</h1>
-      </div>
-      <div>
-        <div className="left-text">
-          <h2>{tbr.name || "My TBR List"}</h2>
+                    <Button
+                        variant="contained"
+                        component={Link}
+                        sx={{
+                            marginRight: 2,
+                            backgroundColor: colors.purple,
+                        }}
+                        to="/ClubsList"
+                    >
+                        Go to Clubs
+                    </Button>
+                </div>
+            </div>
+            <div>
+                <div className="left-text">
+                    <h2>{tbr.name || "My TBR List"}</h2>
+                </div>
+
+                <div className="table-container">
+                    <table
+                        style={{
+                            width: "100%",
+                            borderCollapse: "collapse",
+                            color: darkMode ? "#e0e0e0" : "#333",
+                        }}
+                    >
+                        <thead>
+                            <tr style={{ backgroundColor: darkMode ? "#121212" : "#f5f5f5" }}>
+                                <th
+                                    style={{
+                                        padding: "10px",
+                                        border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                    }}
+                                >
+                                    Name
+                                </th>
+                                <th
+                                    style={{
+                                        padding: "10px",
+                                        border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                    }}
+                                >
+                                    Cover
+                                </th>
+                                <th
+                                    style={{
+                                        padding: "10px",
+                                        border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                    }}
+                                >
+                                    Author
+                                </th>
+                                <th
+                                    style={{
+                                        padding: "10px",
+                                        border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                    }}
+                                >
+                                    Publication Date
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {tbr.tbr.length > 0 ? (
+                                tbr.tbr.map((book) => (
+                                    <tr
+                                        key={book.id}
+                                        style={{
+                                            backgroundColor: darkMode ? "#333" : "#fafafa",
+                                        }}
+                                    >
+                                        <td
+                                            style={{
+                                                padding: "8px",
+                                                border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                            }}
+                                        >
+                                            {book.name}
+                                        </td>
+                                        <td
+                                            style={{
+                                                padding: "8px",
+                                                border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                            }}
+                                        >
+                                            <img src={book.bookCover} alt={book.name} width="50" />
+                                        </td>
+                                        <td
+                                            style={{
+                                                padding: "8px",
+                                                border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                            }}
+                                        >
+                                            {Array.isArray(book.author) ? book.author.join(", ") : book.author}
+                                        </td>
+                                        <td
+                                            style={{
+                                                padding: "8px",
+                                                border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                            }}
+                                        >
+                                            {book.publicationDate}
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td
+                                        colSpan="4"
+                                        style={{
+                                            textAlign: "center",
+                                            padding: "10px",
+                                            border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
+                                        }}
+                                    >
+                                        No books in TBR
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-
-        <div className="table-container">
-        <table
-          style={{
-            width: "100%",
-            borderCollapse: "collapse",
-            color: darkMode ? "#e0e0e0" : "#333",
-          }}
-        >
-          <thead>
-            <tr style={{ backgroundColor: darkMode ? "#121212" : "#f5f5f5" }}>
-              <th
-                style={{
-                  padding: "10px",
-                  border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                }}
-              >
-                Name
-              </th>
-              <th
-                style={{
-                  padding: "10px",
-                  border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                }}
-              >
-                Cover
-              </th>
-              <th
-                style={{
-                  padding: "10px",
-                  border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                }}
-              >
-                Author
-              </th>
-              <th
-                style={{
-                  padding: "10px",
-                  border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                }}
-              >
-                Publication Date
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {tbr.tbr.length > 0 ? (
-              tbr.tbr.map((book) => (
-                <tr
-                  key={book.id}
-                  style={{
-                    backgroundColor: darkMode ? "#333" : "#fafafa",
-                  }}
-                >
-                  <td
-                    style={{
-                      padding: "8px",
-                      border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                    }}
-                  >
-                    {book.name}
-                  </td>
-                  <td
-                    style={{
-                      padding: "8px",
-                      border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                    }}
-                  >
-                    <img src={book.bookCover} alt={book.name} width="50" />
-                  </td>
-                  <td
-                    style={{
-                      padding: "8px",
-                      border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                    }}
-                  >
-                    {Array.isArray(book.author)
-                      ? book.author.join(", ")
-                      : book.author}
-                  </td>
-                  <td
-                    style={{
-                      padding: "8px",
-                      border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                    }}
-                  >
-                    {book.publicationDate}
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td
-                  colSpan="4"
-                  style={{
-                    textAlign: "center",
-                    padding: "10px",
-                    border: `1px solid ${darkMode ? "#444" : "#ccc"}`,
-                  }}
-                >
-                  No books in TBR
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-      </div>
-    </div>
-  );
+    );
 }
